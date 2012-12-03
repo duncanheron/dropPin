fc1c03819bcecac2c0c7230ae5204ad845b53ec3
root <root@duncan-pc.(none)>
2012-12-03 15:42:46 +0000

    created two example files and added default graphic

--- dropPin/dropPin.js
+++ dropPin/dropPin.js
@@ -1,18 +1,18 @@
 /*!
 * dropPin - because image maps are icky
 * http://duncanheron.github.com/dropPin/
-* 
+*
 */
 (function( $ ){
 
 	$.fn.dropPin = function(method) {
-	
+
 		var defaults = {
 		fixedHeight: 500,
 		fixedWidth: 500,
 		dropPinPath: '/js/dropPin/',
-		pin: '/js/dropPin/defaultpin.png',
-		backgroundImage: '',
+		pin: 'dropPin/defaultpin.png',
+		backgroundImage: 'dropPin/access-map.png',
 		backgroundColor: '#9999CC',
 		xoffset : 10,
 		yoffset : 30, //need to change this to work out icon heigh/width then subtract margin from it
@@ -28,89 +28,89 @@
 
 
 
-	var methods = {        
-		init: function(options) {           
-		
+	var methods = {
+		init: function(options) {
+
 			var options =  $.extend(defaults, options);
-			
+
 			this.css({'cursor' : options.cursor, 'background-color' : options.backgroundColor , 'background-image' : "url('"+options.backgroundImage+"')",'height' : options.fixedHeight , 'width' : options.fixedWidth});
-			
+
 			this.on(options.userevent, function (ev) {
-			
+
 				var $img = $(ev.target);
 				var offset = $img.offset();
 				var x = ev.pageX - offset.left;
 				var y = ev.pageY - offset.top;
-				
-				
-				$('.pin').remove();            
-				
+
+
+				$('.pin').remove();
+
 				var xval = (x - options.xoffset);
 				var yval = (y - options.yoffset);
 				var imgC = $('<img class="pin">');
 				imgC.css('top', yval);
 				imgC.css('left', xval);
-				
+
 				imgC.attr('src',  options.pin);
-				
+
 				imgC.appendTo(this);
 				$(options.hiddenXid).val(xval);
-				$(options.hiddenYid).val(yval);	
-		
-			});				
-		
+				$(options.hiddenYid).val(yval);
+
+			});
+
 		},
-		showPin: function(options) {           
-		
+		showPin: function(options) {
+
 			var options =  $.extend(defaults, options);
-			
+
 			this.css({'cursor' : options.cursor, 'background-color' : options.backgroundColor , 'background-image' : "url('"+options.backgroundImage+"')",'height' : options.fixedHeight , 'width' : options.fixedWidth});
-			
+
 			var xval = (options.pinX);
 			var yval = (options.pinY);
 			var imgC = $('<img class="pin">');
 			imgC.css('top', yval);
 			imgC.css('left', xval);
-			
+
 			imgC.attr('src',  options.pin);
-			
+
 			imgC.appendTo(this);
 			$(options.hiddenXid).val(xval);
-			$(options.hiddenYid).val(yval);	
-		
+			$(options.hiddenYid).val(yval);
+
 		},
-		showPins: function(options) {           
-		
+		showPins: function(options) {
+
 			var options =  $.extend(defaults, options);
-			
+
 			this.css({'cursor' : options.cursor, 'background-color' : options.backgroundColor , 'background-image' : "url('"+options.backgroundImage+"')",'height' : options.fixedHeight , 'width' : options.fixedWidth});
-			
+
 			for(var i=0; i < (options.pinDataSet).markers.length; i++)
 			{
 				var dataPin = options.pinDataSet.markers[i];
-				
+
 				var imgC = $('<img rel="/map-content.php?id='+dataPin.id+'" class="pin '+options.pinclass+'" style="top:'+dataPin.ycoord+'px;left:'+dataPin.xcoord+'px;">');
-				imgC.attr('src',  options.pin);	
+				imgC.attr('src',  options.pin);
 				imgC.attr('title',  dataPin.title);
-				
+
 				imgC.appendTo(this);
 			}
-		
+
 		}
-	};    
+	};
 
 	if (methods[method]) {
-	
+
 		return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-	
+
 	} else if (typeof method === 'object' || !method) {
-	
+
 		return methods.init.apply(this, arguments);
-	
+
 	} else {
-	
+
 		alert("method does not exist");
-	
+
 	}
 
 
--- map.php
+++ map.php
@@ -1,46 +1,20 @@
-<?php 
-	$mapItems = array(
-			array("id" => 1, "title" => "map pin 1", "xcoord" => "420","ycoord" => "120"),
-			array("id" => 2, "title" => "map pin 2", "xcoord" => "429","ycoord" => "129")
-			);
-	foreach($mapItems as $map)
-	{
-		$mapPins[] = array(
-				"id" => $map['id'],
-				"title" => $map['title'],					
-				"xcoord" => $map['xcoord'],
-				"ycoord" => $map['ycoord']
-				);
-	}
+<?php
 
 	?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js" ></script></script>
 	<link rel="stylesheet" href="dropPin/dropPin.css" type="text/css" />
-	<script type="text/javascript" src="dropPin/dropPin.js"></script>	
+	<script type="text/javascript" src="dropPin/dropPin.js"></script>
 	<script type="text/javascript">
 	$(document).ready(function() {
-		
-        $('#map').dropPin('showPins',{
-		  	fixedHeight:495,
-		  	fixedWidth:700,
-		  	cursor: 'pointer',
-		  	pinclass: 'qtipinfo',
-		  	pinDataSet: <?php echo '{"markers": '.json_encode($mapPins).'}' ;?>
-	});
 
-	$('#map2').dropPin({
-		  	fixedHeight:495,
-		  	fixedWidth:700,
-		  	cursor: 'crosshair',
-		  	pinclass: 'qtipinfo'					  	
-	});
-		
+	    $('#map').dropPin({
+			  	fixedHeight:413,
+			  	fixedWidth:700,
+			  	cursor: 'crosshair',
+			  	pinclass: 'qtipinfo'
+		});
+
 	});
 	</script>
-	<p>Display pins on a map</p>
+	<p><a href="index.php">Show stored pins on a map</a></p>
 	<div id="map"></div><br />
-	<p>Drop a pin on a map</p>
-
-	<div id="map2"></div>
-
-	<div id="map2"></div>
