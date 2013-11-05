f4d660a26bf565b51c6664fa4cd0d6940446585b
Duncan Heron <duncanheron@Duncans-Mac-mini.local>
2013-11-05 11:04:02 +0000

    added multi drop function to js lib. updated readme to this effect. added hidden fields for easier store to database

---
+++ multi.php
@@ -0,0 +1,20 @@
+<?php
+
+	?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js" ></script></script>
+	<link rel="stylesheet" href="dropPin/dropPin.css" type="text/css" />
+	<script type="text/javascript" src="dropPin/dropPin.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+
+	    $('#map').dropPin('dropMulti',{
+			  	fixedHeight:413,
+			  	fixedWidth:700,
+			  	cursor: 'crosshair',
+			  	pinclass: 'qtipinfo'
+		});
+
+	});
+	</script>
+	<p><a href="index.php">Show stored pins on a map</a></p>
+	<div id="map"></div><br />
